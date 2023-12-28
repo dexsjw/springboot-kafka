01015e5ac52f4bddd7b8db258157e8d74ca5b611
dextersjw <dextersjw@gmail.com>
2023-12-28 08:48:20 +0800

    initial commit

---
+++ web-games-service/src/main/java/springboot/kafka/webgamesservice/WebGamesServiceApplication.java
@@ -0,0 +1,13 @@
+package springboot.kafka.webgamesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebGamesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebGamesServiceApplication.class, args);
+	}
+
+}
---
+++ web-games-service/src/test/java/springboot/kafka/webgamesservice/WebGamesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.kafka.webgamesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebGamesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
