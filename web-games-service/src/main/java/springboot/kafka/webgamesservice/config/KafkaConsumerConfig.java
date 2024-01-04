094f4a14d71f1da20c9aee768a97ff25bfb57b66
Dexter <95396719+dexsjw@users.noreply.github.com>
2024-01-04 18:16:38 +0800

    updated KafkaConsumerConfig with correct configs

--- web-games-service/src/main/java/springboot/kafka/webgamesservice/config/KafkaConsumerConfig.java
+++ web-games-service/src/main/java/springboot/kafka/webgamesservice/config/KafkaConsumerConfig.java
@@ -12,6 +12,9 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import springboot.kafka.webgamesservice.model.TicTacToeBoardState;
 
 @EnableKafka
 @Configuration
@@ -20,20 +23,40 @@ public class KafkaConsumerConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapAddress;
 
+    @Value("${group.id.game.tic-tac-toe}")
+    private String groupIdTicTacToe;
+
+    // @Bean
+    // public ConsumerFactory<String, String> consumerFactory(String groupId) {
+    //      Map<String, Object> configProps = new HashMap<>();
+    //      configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+    //      configProps.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+    //      configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+    //      configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+    //      return new DefaultKafkaConsumerFactory<>(configProps);
+    // }
+
+    // @Bean
+    // public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory(String groupId) {
+    //     ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+    //     factory.setConsumerFactory(consumerFactory(groupId));
+    //     return factory;
+    // }
+
     @Bean
-    public ConsumerFactory<String, String> consumerFactory(String groupId) {
+    public ConsumerFactory<String, TicTacToeBoardState> ticTacToeBoardStateConsumerFactory() {
          Map<String, Object> configProps = new HashMap<>();
          configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-         configProps.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+         configProps.put(ConsumerConfig.GROUP_ID_CONFIG, groupIdTicTacToe);
          configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-         configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+         configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
          return new DefaultKafkaConsumerFactory<>(configProps);
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory(String groupId) {
-        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory(groupId));
+    public ConcurrentKafkaListenerContainerFactory<String, TicTacToeBoardState> ticTacToeBoardStateKafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, TicTacToeBoardState> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(ticTacToeBoardStateConsumerFactory());
         return factory;
     }
     
