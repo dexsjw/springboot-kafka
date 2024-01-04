0dfde46bed78f5e592950d6e702f17191ad4b2eb
Dexter <95396719+dexsjw@users.noreply.github.com>
2024-01-04 17:07:17 +0800

    added coordinates related ProducerFactory and KafkaTemplate

--- web-games-service/src/main/java/springboot/kafka/webgamesservice/config/KafkaProducerConfig.java
+++ web-games-service/src/main/java/springboot/kafka/webgamesservice/config/KafkaProducerConfig.java
@@ -11,6 +11,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import springboot.kafka.webgamesservice.model.PlayerOneCoordinates;
+import springboot.kafka.webgamesservice.model.PlayerTwoCoordinates;
+import springboot.kafka.webgamesservice.model.TicTacToeBoardState;
 
 @Configuration
 public class KafkaProducerConfig {
@@ -31,5 +36,47 @@ public class KafkaProducerConfig {
     public KafkaTemplate<String, String> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
-    
+
+    @Bean
+    public ProducerFactory<String, TicTacToeBoardState> ticTacToeBoardStateProducerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, TicTacToeBoardState> ticTacToeBoardStateKafkaTemplate() {
+        return new KafkaTemplate<>(ticTacToeBoardStateProducerFactory());
+    }
+
+    @Bean
+    public ProducerFactory<String, PlayerOneCoordinates> playerOneCoordinatesProducerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, PlayerOneCoordinates> playerOneCoordinatesKafkaTemplate() {
+        return new KafkaTemplate<>(playerOneCoordinatesProducerFactory());
+    }
+
+    @Bean
+    public ProducerFactory<String, PlayerTwoCoordinates> playerTwoCoordinatesProducerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, PlayerTwoCoordinates> playerTwoCoordinatesKafkaTemplate() {
+        return new KafkaTemplate<>(playerTwoCoordinatesProducerFactory());
+    }
+
 }
