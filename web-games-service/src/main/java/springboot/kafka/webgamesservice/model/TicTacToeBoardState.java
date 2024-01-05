da7127568f88be8d76bdd470b3c4c3d2e8c27527
Dexter <95396719+dexsjw@users.noreply.github.com>
2024-01-05 18:55:38 +0800

    added display function to log col and row

--- web-games-service/src/main/java/springboot/kafka/webgamesservice/model/TicTacToeBoardState.java
+++ web-games-service/src/main/java/springboot/kafka/webgamesservice/model/TicTacToeBoardState.java
@@ -1,10 +1,23 @@
 package springboot.kafka.webgamesservice.model;
 
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.extern.java.Log;
 
+@Log
+@Getter
+@Setter
 @NoArgsConstructor
 public class TicTacToeBoardState extends BoardCoordinate {
 
     private BoardCoordinate[] boardCoordinates;
+
+    public void displayBoardCoordinates() {
+        for (BoardCoordinate coordinate : boardCoordinates) {
+            log.info(coordinate.getColumn());
+            log.info(coordinate.getRow());
+        }
+    }
     
 }
