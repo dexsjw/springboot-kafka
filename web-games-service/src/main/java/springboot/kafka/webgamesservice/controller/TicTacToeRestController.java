d91b5d7d174e7b791fb5c95486785de319a37c07
Dexter <95396719+dexsjw@users.noreply.github.com>
2024-01-05 18:54:51 +0800

    renamed TicTacToeRestController

--- web-games-service/src/main/java/springboot/kafka/webgamesservice/controller/TicTacToeRestController.java
+++
@@ -1,8 +0,0 @@
-package springboot.kafka.webgamesservice.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class TicTacToeRestController {
-    
-}
