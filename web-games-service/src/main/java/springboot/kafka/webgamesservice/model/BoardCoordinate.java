b58f65f4bf23c04a9d3e3de2e132ebc6adbae3de
Dexter <95396719+dexsjw@users.noreply.github.com>
2024-01-05 18:55:09 +0800

    added getters and setters

--- web-games-service/src/main/java/springboot/kafka/webgamesservice/model/BoardCoordinate.java
+++ web-games-service/src/main/java/springboot/kafka/webgamesservice/model/BoardCoordinate.java
@@ -1,7 +1,11 @@
 package springboot.kafka.webgamesservice.model;
 
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
 @NoArgsConstructor
 public class BoardCoordinate {
 
